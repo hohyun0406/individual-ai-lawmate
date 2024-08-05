53429d360d9efae50341932d73f1dd17ba4ef911
hohyun0406 <hohyun0406@gmail.com>
2024-08-05 22:18:47 +0900

    feat: add home-work-space(zoup)

---
+++ mlsetting/zoup/initialize.py
@@ -0,0 +1,5 @@
+import pip
+from subprocess import call
+with open('requirements.txt', encoding='utf-8-sig',mode='r') as file:
+    for library_name in file.readlines():
+        call("conda install -y " + library_name, shell=True)
